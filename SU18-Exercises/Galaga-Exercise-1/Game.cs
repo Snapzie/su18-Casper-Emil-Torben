c7247b818ba4d468ea7f5e2b9d787b080626c968
kaapsi <casper@bresdahl.dk>
2018-02-27 20:58:14 +0100

    Added Game.cs to repo

---
+++ SU18-Exercises/Galaga-Exercise-1/Game.cs
@@ -0,0 +1,22 @@
+﻿using DIKUArcade;
+
+namespace Galaga_Exercise_1 {
+    public class Game {
+        private Window win;
+
+        public Game() {
+// look at the Window.cs file for possible constructors.
+// We recommend using 500 × 500 as window dimensions,
+// which is most easily done using a predefined aspect ratio.
+            win = new Window("Cool Game", 500, 500);
+        }
+
+        public void GameLoop() {
+            while (win.IsRunning()) {
+                win.PollEvents();
+                win.Clear();
+                win.SwapBuffers();
+            }
+        }
+    }
+}
