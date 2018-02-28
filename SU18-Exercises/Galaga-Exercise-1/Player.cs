ca31de7d58795c63b9529f701e98f20dac16d5fa
kaapsi <casper@bresdahl.dk>
2018-03-01 00:33:46 +0100

    Renamed field in player class

--- SU18-Exercises/Galaga-Exercise-1/Player.cs
+++ SU18-Exercises/Galaga-Exercise-1/Player.cs
@@ -7,47 +7,47 @@ using DIKUArcade.Math;
 namespace Galaga_Exercise_1 {
     public class Player : IGameEventProcessor<object> {
         private GameEventBus<object> eventBus;
-        private Entity player;
+        private Entity self;
         private float movementSpeed = 0.01f;
 
         public Player() {
             eventBus = new GameEventBus<object>();
             eventBus.Subscribe(GameEventType.PlayerEvent, this);
             
-            player = new Entity(
+            self = new Entity(
                 new DynamicShape(new Vec2F(0.45f, 0.1f), new Vec2F(0.1f, 0.1f)), 
                 new Image(Path.Combine("Assets", "Images", "Player.png")));
         }
 
         public void Move() {
-            player.Shape.Move();
-            if (((DynamicShape) (player.Shape)).Position.X > 0.9) {
+            self.Shape.Move();
+            if (((DynamicShape) (self.Shape)).Position.X > 0.9) {
                 //Console.WriteLine((((DynamicShape) (player.Shape)).Position.X));
-                ((DynamicShape) (player.Shape)).Position.X = 0.9f;
-            }else if (((DynamicShape) (player.Shape)).Position.X < 0.0) {
+                ((DynamicShape) (self.Shape)).Position.X = 0.9f;
+            }else if (((DynamicShape) (self.Shape)).Position.X < 0.0) {
                 //Console.WriteLine((((DynamicShape) (player.Shape)).Position.X));
-                ((DynamicShape) (player.Shape)).Position.X = 0.0f;
+                ((DynamicShape) (self.Shape)).Position.X = 0.0f;
             }
         }
 
         public void Render() {
-            player.RenderEntity();
+            self.RenderEntity();
         }
 
         public void MoveLeft() {
-            ((DynamicShape) (player.Shape)).Direction.X = -movementSpeed;
+            ((DynamicShape) (self.Shape)).Direction.X = -movementSpeed;
         }
         
         public void MoveRight() {
-            ((DynamicShape) (player.Shape)).Direction.X = movementSpeed; 
+            ((DynamicShape) (self.Shape)).Direction.X = movementSpeed; 
+        }
+
+        public void KeyRelease() {
+            ((DynamicShape) (self.Shape)).Direction.X = 0.0f;
         }
         
         public void ProcessEvent(GameEventType eventType, GameEvent<object> gameEvent) {
             
-        }
-
-        public void KeyRelease() {
-            ((DynamicShape) (player.Shape)).Direction.X = 0.0f;
         }
     }
 }
