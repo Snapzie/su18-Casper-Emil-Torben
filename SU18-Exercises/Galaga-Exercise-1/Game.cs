3a59dfe940285005a8dc9bc1e12c71d08023dd7c
kaapsi <casper@bresdahl.dk>
2018-02-28 17:35:21 +0100

    Added player

--- SU18-Exercises/Galaga-Exercise-1/Game.cs
+++ SU18-Exercises/Galaga-Exercise-1/Game.cs
@@ -1,14 +1,23 @@
-﻿using DIKUArcade;
+﻿using System.IO;
+using DIKUArcade;
+using DIKUArcade.Entities;
+using DIKUArcade.Graphics;
+using DIKUArcade.Math;
 
 namespace Galaga_Exercise_1 {
     public class Game {
         private Window win;
+        private Entity player;
 
         public Game() {
 // look at the Window.cs file for possible constructors.
 // We recommend using 500 × 500 as window dimensions,
 // which is most easily done using a predefined aspect ratio.
             win = new Window("Cool Game", 500, 500);
+
+            player = new Entity(
+                new DynamicShape(new Vec2F(0.45f, 0.1f), new Vec2F(0.1f, 0.1f)), 
+                new Image(Path.Combine("Assets", "Images", "Player.png")));
         }
 
         public void GameLoop() {
@@ -16,6 +25,8 @@ namespace Galaga_Exercise_1 {
                 win.PollEvents();
                 win.Clear();
                 win.SwapBuffers();
+                
+                player.RenderEntity();
             }
         }
     }
