2fcb23f0632a3d7f9c61a0969cf4df7987037bbe
Emil2468 <emh@calaha.dk>
2018-02-28 17:28:49 +0100

    Lavet opg 3.3 der ikke virker før 3.2 er lavet

--- SU18-Exercises/Galaga-Exercise-1/Game.cs
+++ SU18-Exercises/Galaga-Exercise-1/Game.cs
@@ -1,21 +1,83 @@
-﻿using DIKUArcade;
+﻿using System.Collections.Generic;
+using System.Media;
+using DIKUArcade;
+using DIKUArcade.EventBus;
 
 namespace Galaga_Exercise_1 {
-    public class Game {
+    public class Game : IGameEventProcessor<object> {
         private Window win;
+        private GameEventBus<object> eventBus;
 
         public Game() {
-// look at the Window.cs file for possible constructors.
-// We recommend using 500 × 500 as window dimensions,
-// which is most easily done using a predefined aspect ratio.
+            // look at the Window.cs file for possible constructors.
+            // We recommend using 500 × 500 as window dimensions,
+            // which is most easily done using a predefined aspect ratio.
             win = new Window("Cool Game", 500, 500);
+            eventBus = new GameEventBus<object>();
+            eventBus.InitializeEventBus(new List<GameEventType>() {
+                GameEventType.InputEvent, // key press / key release
+                GameEventType.WindowEvent, // messages to the window
+                GameEventType.PlayerEvent // commands issued to the player object,
+            });
+            // e.g. move, destroy, receive health, etc.
+            win.RegisterEventBus(eventBus);
+            eventBus.Subscribe(GameEventType.InputEvent, this);
+            eventBus.Subscribe(GameEventType.WindowEvent, this);
         }
 
         public void GameLoop() {
             while (win.IsRunning()) {
+                eventBus.ProcessEvents();
                 win.PollEvents();
                 win.Clear();
                 win.SwapBuffers();
+                player.Move();
+            }
+        }
+
+        public void KeyPress(string key) {
+            switch (key) {
+                case "KEY_ESCAPE":
+                    eventBus.RegisterEvent(
+                            GameEventFactory<object>.CreateGameEventForAllProcessors(
+                                GameEventType.WindowEvent, this, "CLOSE_WINDOW", "", ""));
+                    break;
+                case "KEY_LEFT":
+                    player.Shape.Direction.X = -0.0001f;
+                    break;
+                case "KEY_RIGTH":
+                    player.Shape.Direction.X = 0.0001f;
+                    break;
+            }
+
+            // match on e.g. "KEY_UP", "KEY_1", "KEY_A", etc.
+            // TODO: use this method to start moving your player object
+            player.Shape.Direction.X = 0.0001f; // choose a fittingly small number
+        }
+
+        public void KeyRelease(string key) {
+            // match on e.g. "KEY_UP", "KEY_1", "KEY_A", etc.
+            player.Shape.Direction.X = 0.0f;
+        }
+
+        public void ProcessEvent(GameEventType eventType, GameEvent<object> gameEvent) {
+            if (eventType == GameEventType.WindowEvent) {
+                switch (gameEvent.Message) {
+                case "CLOSE_WINDOW":
+                    win.CloseWindow();
+                    break;
+                default:
+                    break;
+                }
+            } else if (eventType == GameEventType.InputEvent) {
+                switch (gameEvent.Parameter1) {
+                case "KEY_PRESS":
+                    KeyPress(gameEvent.Message);
+                    break;
+                case "KEY_RELEASE":
+                    KeyRelease(gameEvent.Message);
+                    break;
+                }
             }
         }
     }
