2b69230df0a9ee3302eae708dc4aa9c3ebc4b8f1
kaapsi <casper@bresdahl.dk>
2018-03-21 21:53:39 +0100

    Galaga 2 compile error fix

--- SU18-Exercises/Galaga-Exercise-3/MovementStrategy/IMovementStrategy.cs
+++
@@ -1,9 +0,0 @@
-﻿using DIKUArcade.Entities;
-using Galaga_Exercise_3.GalagaEntities;
-
-namespace Galaga_Exercise_3.MovementStrategy {
-    public interface IMovementStrategy {
-        void MoveEnemy(Enemy enemy);
-        void MoveEnemies(EntityContainer<Enemy> enemies);
-    }
-}
--- SU18-Exercises/Galaga-Exercise-3/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Galaga_Exercise_3 {
-    internal class Program {
-        public static void Main(string[] args) {
-            Game game = new Game();
-            game.GameLoop();
-        }
-    }
-}
