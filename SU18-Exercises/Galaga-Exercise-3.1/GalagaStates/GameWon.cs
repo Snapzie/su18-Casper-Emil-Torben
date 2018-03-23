6f96be710b8d4e636b0d48164ff93611f5937090
kaapsi <casper@bresdahl.dk>
2018-03-23 11:21:15 +0100

    Various fixes and improvements including GameWon, GameLost, and fix to Squareformation so it stays inside play area when ZigZagDown is applied

---
+++ SU18-Exercises/Galaga-Exercise-3.1/GalagaStates/GameWon.cs
@@ -0,0 +1,53 @@
+﻿using System.Drawing;
+using DIKUArcade.EventBus;
+using DIKUArcade.Graphics;
+using DIKUArcade.Math;
+using DIKUArcade.State;
+using Galaga_Exercise_3._1.GalagaGame;
+
+namespace Galaga_Exercise_3._1.GalagaStates {
+    public class GameWon : IGameState {
+        private static GameWon instance = null;
+        private Text[] GameWonTexts;
+
+        private GameWon() {
+            GameWonTexts = new Text[] {
+                new Text("Game Won!", new Vec2F(0.4f, 0.4f), new Vec2F(0.3f, 0.3f)),
+                new Text("Enter to continue", new Vec2F(0.36f, 0.3f), new Vec2F(0.3f, 0.3f))
+            };  
+        }
+        
+        public static GameWon GetInstance() {
+            return GameWon.instance ?? (GameWon.instance = new GameWon());
+        }
+        
+        public void GameLoop() {
+            this.RenderState();
+        }
+
+        public void InitializeGameState() {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateGameLogic() {
+            throw new System.NotImplementedException();
+        }
+
+        public void RenderState() {
+            foreach (Text text in this.GameWonTexts) {
+                text.SetColor(Color.Blue);
+                text.RenderText();
+            }
+        }
+
+        public void HandleKeyEvent(string keyValue, string keyAction) {
+            if (keyAction == "KEY_RELEASE") {
+                if (keyValue == "KEY_ENTER") {
+                    GalagaBus.GetBus().RegisterEvent(
+                        GameEventFactory<object>.CreateGameEventForAllProcessors(
+                            GameEventType.GameStateEvent, this, "STATE_CHANGE", "MainMenu", ""));   
+                }
+            }
+        }
+    }
+}
--- SU18-Exercises/Galaga-Exercise-3.1/Player.cs
+++ SU18-Exercises/Galaga-Exercise-3.1/Player.cs
@@ -6,38 +6,38 @@ using DIKUArcade.Math;
 
 namespace Galaga_Exercise_3._1 {
     public class Player : IGameEventProcessor<object> {
-        public Entity Self { get; private set; }
+        public Entity PlayerEntity { get; private set; }
         private float movementSpeed = 0.01f;
 
         public Player() {
-            Self = new Entity(
+            PlayerEntity = new Entity(
                 new DynamicShape(new Vec2F(0.45f, 0.1f), new Vec2F(0.1f, 0.1f)), 
                 new Image(Path.Combine("Assets", "Images", "Player.png")));
         }
 
         public void Move() {
-            Self.Shape.Move();
-            if (Self.Shape.Position.X > 0.9) {
-                Self.Shape.Position.X = 0.9f;
-            }else if (Self.Shape.Position.X < 0.0) {
-                Self.Shape.Position.X = 0.0f;
+            PlayerEntity.Shape.Move();
+            if (PlayerEntity.Shape.Position.X > 0.9) {
+                PlayerEntity.Shape.Position.X = 0.9f;
+            }else if (PlayerEntity.Shape.Position.X < 0.0) {
+                PlayerEntity.Shape.Position.X = 0.0f;
             }
         }
 
         public void Render() {
-            Self.RenderEntity();
+            PlayerEntity.RenderEntity();
         }
 
         public void MoveLeft() {
-            ((DynamicShape) (Self.Shape)).Direction.X = -movementSpeed;
+            ((DynamicShape) (PlayerEntity.Shape)).Direction.X = -movementSpeed;
         }
         
         public void MoveRight() {
-            ((DynamicShape) (Self.Shape)).Direction.X = movementSpeed; 
+            ((DynamicShape) (PlayerEntity.Shape)).Direction.X = movementSpeed; 
         }
 
         public void KeyRelease() {
-            ((DynamicShape) (Self.Shape)).Direction.X = 0.0f;
+            ((DynamicShape) (PlayerEntity.Shape)).Direction.X = 0.0f;
         }
         
         public void ProcessEvent(GameEventType eventType, GameEvent<object> gameEvent) {
