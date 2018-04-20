a1d273081007a94e969b1a513a9da9a800adf11a
Emil2468 <emh@calaha.dk>
2018-04-20 12:28:19 +0200

    Irrelevant changes before pull

---
+++ SU18-Exercises/Galaga-Exercise-3/MovementStrategy/Down.cs
@@ -0,0 +1,18 @@
+﻿using DIKUArcade.Entities;
+using Galaga_Exercise_3.GalagaEntities;
+
+namespace Galaga_Exercise_3.MovementStrategy {
+    public class Down : IMovementStrategy {
+        private float s = 0.0003f;
+        
+        public void MoveEnemy(Enemy enemy) {
+            enemy.Position.Y -= s;
+        }
+
+        public void MoveEnemies(EntityContainer<Enemy> enemies) {
+            foreach (Enemy enemy in enemies) {
+                MoveEnemy(enemy);
+            }
+        }
+    }
+}
