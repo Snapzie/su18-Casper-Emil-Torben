182bdd672484be72fc205aef0438c33afb61eb30
Emil2468 <emh@calaha.dk>
2018-04-25 13:35:45 +0200

    LAvet flere tabeller

---
+++ SU18-Exercises/Galaga-Exercise-3/MovementStrategy/NoMove.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DIKUArcade.Entities;
+using Galaga_Exercise_3.GalagaEntities;
+
+namespace Galaga_Exercise_3.MovementStrategy {
+    public class NoMove : IMovementStrategy {
+
+        public void MoveEnemy(Enemy enemy) {
+            return;
+        }
+
+        public void MoveEnemies(EntityContainer<Enemy> enemies) {
+            //Returns immediately to avoid movement
+            return;
+        }
+    }
+}
---
+++ SU18-Exercises/Galaga-Exercise-3/MovementStrategy/ZigZagDown.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using DIKUArcade.Entities;
+using Galaga_Exercise_3.GalagaEntities;
+
+namespace Galaga_Exercise_3.MovementStrategy {
+    public class ZigZagDown : IMovementStrategy {
+        private float s = 0.0003f;
+        private float p = 0.045f;
+        private float a = 0.005f;
+
+        public void MoveEnemy(Enemy enemy) {
+            enemy.Position.Y -= s;
+            enemy.Position.X = enemy.InitPosition.X + this.a * ((float) Math.Sin((2 * Math.PI * 
+                 (0.9f - (enemy.Position.Y - s))) / p));
+        }
+
+        public void MoveEnemies(EntityContainer<Enemy> enemies) {
+            foreach (Enemy enemy in enemies) {
+                MoveEnemy(enemy);
+            }
+        }
+    }
+}
