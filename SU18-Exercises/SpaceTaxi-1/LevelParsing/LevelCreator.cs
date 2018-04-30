1627945e0ea69aa6b4457418c064ae0213deb21f
impe98 <impe98@gmail.com>
2018-04-30 13:25:41 +0200

    Level Creator

---
+++ SU18-Exercises/SpaceTaxi-1/LevelParsing/LevelCreator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using DIKUArcade.Entities;
+
+namespace SpaceTaxi_1.LevelParsing {
+    public class LevelCreator {
+        private static int level;
+        private static LevelsKeeper levelKeeper = LevelsKeeper.Instance;
+        private Level entityContainer = levelKeeper.GetLevel(level);
+    }
+   
+}
