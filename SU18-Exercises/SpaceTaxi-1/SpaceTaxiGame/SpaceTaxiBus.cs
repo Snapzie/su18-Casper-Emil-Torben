d08b623efb0a4bdd4f5bc5a979207f203cfe9ccd
kaapsi <casper@bresdahl.dk>
2018-05-04 09:29:17 +0200

    Added statemachine

---
+++ SU18-Exercises/SpaceTaxi-1/SpaceTaxiGame/SpaceTaxiBus.cs
@@ -0,0 +1,12 @@
+﻿using DIKUArcade.EventBus;
+
+namespace SpaceTaxi_1.SpaceTaxiGame {
+    public static class SpaceBus {
+        private static GameEventBus<object> eventBus;
+        
+        public static GameEventBus<object> GetBus() {
+            return SpaceBus.eventBus ?? (SpaceBus.eventBus =
+                       new GameEventBus<object>());
+        }
+    }
+}
