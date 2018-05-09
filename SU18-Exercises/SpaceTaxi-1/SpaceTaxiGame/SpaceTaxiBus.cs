c408565d2536a4a19d57ed45d4055ec0b24c3cf4
kaapsi <casper@bresdahl.dk>
2018-05-09 10:08:28 +0200

    Spacetaxi2 created

--- SU18-Exercises/SpaceTaxi-1/SpaceTaxiGame/SpaceTaxiBus.cs
+++ SU18-Exercises/SpaceTaxi-1/SpaceTaxiGame/SpaceTaxiBus.cs
@@ -4,6 +4,10 @@ namespace SpaceTaxi_1.SpaceTaxiGame {
     public static class SpaceBus {
         private static GameEventBus<object> eventBus;
         
+        /// <summary>
+        /// Instantiates or returns the SpaceBus singleton
+        /// </summary>
+        /// <returns>Returns an instance of SpaceBus</returns>
         public static GameEventBus<object> GetBus() {
             return SpaceBus.eventBus ?? (SpaceBus.eventBus =
                        new GameEventBus<object>());
