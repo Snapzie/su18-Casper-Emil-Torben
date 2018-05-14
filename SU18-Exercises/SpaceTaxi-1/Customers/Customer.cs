3a7f7fac280eee09dd33cabf7d2e70b18e1c3da3
impe98 <impe98@gmail.com>
2018-05-14 15:33:33 +0200

    Customer spawn and Despawn

--- SU18-Exercises/SpaceTaxi-1/Customers/Customer.cs
+++ SU18-Exercises/SpaceTaxi-1/Customers/Customer.cs
@@ -1,22 +1,54 @@
-﻿namespace SpaceTaxi_1.Customers {
-    public class Customer {
-        private string Name;
-        private int SpawnTime;
-        private char SpawnPlatform;
-        private string DestinationPlatform;
-        private int TimeToDropOff;
-        private int Points;
+﻿using System.IO;
+using DIKUArcade.Entities;
+using DIKUArcade.Graphics;
+using SpaceTaxi_1.LevelParsing;
 
+namespace SpaceTaxi_1.Customers {
+    public class Customer : ICustomer {
+        private string name;
+        private int spawnTime;
+        private char spawnPlatform;
+        private string destinationPlatform;
+        private int timeToDropOff;
+        private int points;
+        private string imageString;
+        private int posX;
+        private int posY;
+        public Entity Entity;
+        public Level level;
+        
+        
+    
         public Customer(string name, int spawnTime, char spawnPlatform, string destinationPlatform, int timeToDropOff,
-            int points) {
-            Name = name;
-            SpawnTime = spawnTime;
-            SpawnPlatform = spawnPlatform;
-            DestinationPlatform = destinationPlatform;
-            TimeToDropOff = timeToDropOff;
-            Points = points;
+            int points, int posX, int posY) {
+            this.name = name;
+            this.spawnTime = spawnTime;
+            this.spawnPlatform = spawnPlatform;
+            this.destinationPlatform = destinationPlatform;
+            this.timeToDropOff = timeToDropOff;
+            this.points = points;
+            this.posX = posX;
+            this.posY = posY;
 
         }
-        
+
+        public void Spawn(Customer customer) {
+
+            EntityCreator entityCreator = new EntityCreator();
+            /// One is subtracted to ensure the customer lands upon the platform,
+            /// which is equal to subtracting 1 from it's y-coordinate, as the y-coordinate
+            /// is given at the base of the object. 
+            entityCreator.CreateEntity(posY - 1, posX, new Image(Path.Combine("Assets", "Images", imageString)));
+            level.AddCustomer(customer);
+        }
+
+        public void Despawn(Customer customer) {
+            
+            level.RemoveCustomer(customer);
+        }
+
+        public void GivePoints() {
+            throw new System.NotImplementedException();
+        }
     }
 }
