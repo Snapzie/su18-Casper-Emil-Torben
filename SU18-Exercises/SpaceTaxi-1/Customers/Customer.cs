573be9d401287ad1c021ee07d1c88736b686100d
impe98 <impe98@gmail.com>
2018-05-14 14:39:48 +0200

    Customer class underway

--- SU18-Exercises/SpaceTaxi-1/Customers/Customer.cs
+++ SU18-Exercises/SpaceTaxi-1/Customers/Customer.cs
@@ -1,5 +1,22 @@
 ﻿namespace SpaceTaxi_1.Customers {
     public class Customer {
+        private string Name;
+        private int SpawnTime;
+        private char SpawnPlatform;
+        private string DestinationPlatform;
+        private int TimeToDropOff;
+        private int Points;
+
+        public Customer(string name, int spawnTime, char spawnPlatform, string destinationPlatform, int timeToDropOff,
+            int points) {
+            Name = name;
+            SpawnTime = spawnTime;
+            SpawnPlatform = spawnPlatform;
+            DestinationPlatform = destinationPlatform;
+            TimeToDropOff = timeToDropOff;
+            Points = points;
+
+        }
         
     }
 }
