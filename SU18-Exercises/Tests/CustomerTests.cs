77258fc1a0c4906e9667bc1fb2e438c71f31515b
Emil2468 <emh@calaha.dk>
2018-05-22 18:54:11 +0200

    translator returnerer nu array af customers

---
+++ SU18-Exercises/Tests/CustomerTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+using DIKUArcade;
+using DIKUArcade.Entities;
+using DIKUArcade.Graphics;
+using DIKUArcade.Math;
+using SpaceTaxi_1;
+using SpaceTaxi_1.LevelParsing;
+using SpaceTaxi_1.MockUps;
+
+namespace Tests {
+    [TestFixture]
+    public class CustomerTests {
+        [SetUp]
+        public void SetUp() {
+            var dir = Path.GetDirectoryName(typeof(LevelLoader).Assembly.Location);
+            Environment.CurrentDirectory = dir;
+        }
+        [Test]
+        public void TestPosition() {
+            
+        }
+    }
+}
